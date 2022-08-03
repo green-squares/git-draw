84d915c4e9fcaf07fde554256e815d80fba8490c
green-squares <justin889103@gmail.com>
2022-08-03 18:19:49 -0500

    0b65a55338b1ae47542bd0ec4bca167e47ef20ae4c6e672d47377d048ed55498

--- draw.py
+++ draw.py
@@ -2,14 +2,43 @@
 
 # Justus Languell 2022
 
-user = "green-squares"
-pat = "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"
-repo = "git-draw"
+import random
+import hashlib
+import subprocess
+import os
 
-def push(username, password, user, repo):
-    return f"git push https://'{username}':'{password}'@github.com/{user}/{repo}.git"
 
-def commit(username, password, repo):
+USERNAME = "green-squares"
+EMAIL = "justin889103@gmail.com"
+PERSONAL_ACCESS_TOKEN = "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"
+REPO = "git-draw"
 
-print(push(user, pat, user, repo))
+class Git:
+    def __init__(self, username, email, personal_access_token, repo_owner, repo_name):
+        self.username = username
+        self.email = email
+        self.personal_access_token = personal_access_token
+        self.repo_owner = repo_owner
+        self.repo_name = repo_name
+        
+    def add(self):
+        os.system(f"git add .")
+    
+    def commit(self, date, message):
+        os.system(f'GIT_COMMITTER_NAME="{self.username}" GIT_COMMITTER_EMAIL="{self.email}" git commit --author="green-squares <{self.email}>" -a -m "{message}"')
 
+    def push(self):
+        os.system(f"git push https://'{self.user}':'{self.personal_access_token}'@github.com/{self.repo_owner}/{self.repo_name}.git")
+    
+def write_random_data():
+    s = hashlib.sha256(bytes(str(random.randint(1e5, 1e6 - 1)),'utf-8')).hexdigest()
+    open("random.txt", "a").write(s + "\n")
+    return s
+
+if __name__ == "__main__":
+    git = Git(USERNAME, EMAIL, PERSONAL_ACCESS_TOKEN, USERNAME, REPO)
+
+    s = write_random_data()
+    git.add()
+    git.commit(None, s)
+    git.push()
