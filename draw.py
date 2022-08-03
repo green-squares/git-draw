0d26e6dc26baf734140eaa8e1b489c4bfd65b79b
green-squares <justin889103@gmail.com>
2022-08-03 18:04:24 -0500

    Another commit

--- draw.py
+++ draw.py
@@ -1,7 +1,15 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
 
 # Justus Languell 2022
 
+user = "green-squares"
+pat = "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"
+repo = "git-draw"
 
+def push(username, password, user, repo):
+    return f"git push https://'{username}':'{password}'@github.com/{user}/{repo}.git"
 
+def commit(username, password, repo):
+
+print(push(user, pat, user, repo))
 
